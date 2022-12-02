785df99989163df5f267447442b33896c954da70
Arktic-Panda <65855745+Arktic-Panda@users.noreply.github.com>
2022-12-02 14:38:57 +0100

    Stupid Commit

---
+++ Assets/Scripts/CoinPickUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickUp : MonoBehaviour
+{
+   // private GameManager gameLevelManager;
+   // public int coinValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       // gameLevelManager.AddCoins(coinValue);
+        Destroy(gameObject);
+
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private int coins;
+    //public PickUp pickUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // pickUp = FindObjectOfType<PickUp> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddCoins (int numberOfCoins)
+    {
+        coins += numberOfCoins;
+        
+      /*  musicBar.SetMusic(coins);
+        sceneChange.CoinValue(coins);
+        loadScene.CoinValue(coins);
+        loadStory.CoinValue(coins);
+
+        */
+        
+    }
+}
